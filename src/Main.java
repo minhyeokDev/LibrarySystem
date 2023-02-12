c63594beb2ed96b4e9fecdd090c57e54245f9f6b
minhyeok <minhyeok.dev@gmail.com>
2023-02-12 22:26:05 +0900

    첫 커밋

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-class Book{
+class Book {
 
     int number;
 }
-class Library{
+
+class Library {
 
     String book;
 
@@ -13,6 +14,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("대여할 책의 번호를 입력하세요.");
-
+        System.out.println("1.");
     }
 }
