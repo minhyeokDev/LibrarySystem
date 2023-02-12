0a4ba98c6bcc475b758b7e3e5be44e2d1509d5fd
minhyeok <minhyeok.dev@gmail.com>
2023-02-12 22:19:16 +0900

    클래스 생성

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+class Book{
+
+    int number;
+}
+class Library{
+
+    String book;
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("대여할 책의 번호를 입력하세요.");
+
+    }
+}
