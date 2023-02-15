5abaf7aa8e1ff2e606672b8f109852d8455b273e
minhyeok <minhyeok.dev@gmail.com>
2023-02-15 21:14:24 +0900

    number 구현

--- src/bookpackage/Book.java
+++ src/bookpackage/Book.java
@@ -5,9 +5,9 @@ public class Book {
     private String name;
     private boolean isBorrowed; //대여 중인지 아닌지
 
-    Book(String name) {
+    Book(String name) { //생성자
         this.name = name;
-        this.isBorrowed = false;
+        this.isBorrowed = false; // 대여X 인 상태 false
     }
 
     boolean BookBorrow() // 책 대여 시스템
@@ -15,15 +15,15 @@ public class Book {
         //if(this.number == number) {
         if (this.isBorrowed == false) {
             System.out.println("정상적으로 대여가 완료되었습니다.");
-            return this.isBorrowed = true; //대여 완료 후 책 대여 불가 상태로 return
+            return this.isBorrowed = true; //대여 완료 후 책 대여 불가 상태(true)로 return
         } else if (this.isBorrowed == true) {
             System.out.println("대여 중인 책은 대여할 수 없습니다.");
-            return this.isBorrowed = true; //반납 시스템은 없으므로 false 하고 끝
+            return this.isBorrowed = true; //반납 시스템은 없으므로 true 하고 끝
         }
         return true;
     }
 
-    String status() { //대여 상태 확인
+    String BookStatus() { //대여 상태 확인
         if (this.isBorrowed == false) {
             return "대여 가능";
         } else {
@@ -31,7 +31,9 @@ public class Book {
         }
     }
 
-    String BookName() {
+    String BookName() { //책 이름 확인
         return this.name;
     }
+
+
 }
--- src/bookpackage/Library.java
+++ src/bookpackage/Library.java
@@ -2,31 +2,40 @@ package bookpackage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Library {
 
     private List<Book> books;
 
-    public Library(List<Book> books) {
+    public Library(List<Book> books) { //생성자
         this.books = books;
     }
 
-    int getNumber(int n) { //책 번호 출력
-        String name = books.get(n).BookName();
-        System.out.println(name);
-        int number = books.indexOf(name);
-        return number;
+    int getNumber(int number) { //책 번호 출력 equals 이용?
+        String name = books.get(number).BookName();
+        if( name.equals("클린코드(Clean Code)"))
+        {
+            return 1;
+        } else if (name.equals("객체지향의 사실과 오해")) {
+            return 2;
+
+        } else if (name.equals("테스트 주도 개발(TDD)")) {
+            return 3;
+        }
+        return 0;
     }
+
 
     String getName(int number) { //책 이름 출력
         return books.get(number).BookName();
     }
 
     String getStatus(int number) { //책 대여 상태 출력
-        return books.get(number).status();
+        return books.get(number).BookStatus();
     }
 
-    boolean getBorrow(int number) { // 책 대여시
+    boolean getBorrow(int number) { // 책 대여 작동
         int renumber = number - 1;
         return books.get(renumber).BookBorrow();
     }
